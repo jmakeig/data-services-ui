d4cd25c65499a99df30e3e820fed35776805d933
Justin Makeig <jmakeig@marklogic.com>
2018-11-18 09:25:25 -0800

    Gets tests working in VSCode and Gradle

---
+++ my-app/src/test/java/com/acme/DataServiceTest.java
@@ -0,0 +1,12 @@
+package com.acme;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DataServiceTest {
+  @Test
+  public void testRuleDefinitions() {
+    assertTrue(true);
+  }
+}

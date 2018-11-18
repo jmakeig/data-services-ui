b9af9e99e415aa6741e910873f5a997ad3eeab72
Justin Makeig <jmakeig@marklogic.com>
2018-11-18 15:54:18 -0800

    Working end-to-end data service unit test

--- my-app/src/main/ml-modules/root/helloWorld/helloWorld.sjs
+++ my-app/src/main/ml-modules/root/helloWorld/helloWorld.sjs
@@ -5,4 +5,8 @@ var greeting; // instance of xs.string
 var frequency; // instance of xs.unsignedLong
 
 // TODO:  produce the xs.string output from the input variables
-
+const items = [];
+for (let i = 0; i < frequency; i++) {
+  items.push(greeting);
+}
+items.join(' ');
--- my-app/src/test/java/com/acme/DataServiceTest.java
+++ my-app/src/test/java/com/acme/DataServiceTest.java
@@ -1,14 +1,15 @@
 package com.acme;
 
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.DatabaseClientFactory;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import com.marklogic.DatabaseClient;
-import com.marklogic.DatabaseClientFactory;
 
 public class DataServiceTest {
   @Test
   public void testRuleDefinitions() {
-    DatabaseClient client = DatabaseClientFactory.newClient("localhost", 8003, "username", "password",
-        Authentication.DIGEST);
-    // HelloWorld.on();
+    DatabaseClient db = DatabaseClientFactory.newClient("localhost", 8099,
+        new DatabaseClientFactory.DigestAuthContext("admin", "admin"));
+    assertEquals("Hey! Hey!", HelloWorld.on(db).whatsUp("Hey!", 2L));
   }
 }
